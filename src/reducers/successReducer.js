47f1e12823b19534a9be6ec3a57e1b55130a02a9
Catevika <dominique.bello@outlook.fr>
2020-06-18 14:11:08 +0200

    Set up Redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ *
+ * @param {array} state - Array of Guessed Words
+ * @param {object} action - Action to be reduced
+ * @returns {boolean} - New success state
+ */
+
+export default (state, action) => {
+	return null;
+};
